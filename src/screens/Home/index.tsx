0983c84dbf3f4817107579338ae0210ea3de551c
Bruno Souza <brunoanunciacaosouza@gmail.com>
2024-10-01 16:53:16 -0300

    adicionando participante

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,57 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+
+import {
+  Text,
+  TextInput,
+  View,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
 
 import { styles } from "../styles";
+import Participant from "../../components/Participant";
 
 export default function Home() {
+  const [participants, setParticipants] = useState<string[]>([]);
+  const [participantName, setParticipantName] = useState("");
+
+  function handleParticipantAdd() {
+    setParticipants((prevState) => [...prevState, participantName]);
+
+    setParticipantName("");
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Nome do evento: </Text>
+      <Text style={styles.eventName}>Nome do evento: </Text>
+
+      <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2024</Text>
+
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor="#6b6b6b"
+          value={participantName}
+          onChangeText={setParticipantName}
+        />
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
+
+      <FlatList
+        data={participants}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <Participant name={item} />}
+        showsVerticalScrollIndicator={false}
+        ListEmptyComponent={() => (
+          <Text style={styles.listEmptyText}>
+            Ninguém chegou no evento ainda? Adicione participantes a sua lista
+            de presença.
+          </Text>
+        )}
+      />
     </View>
   );
 }
